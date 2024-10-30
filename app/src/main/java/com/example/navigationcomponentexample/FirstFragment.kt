ef21be10ac74d75d8ac754ddb6e5001010b376ab
CrisLozanoTrochez <bycrispapo13@gmail.com>
2024-10-29 21:11:33 -0500

    Primera Navegación Básica

--- app/src/main/java/com/example/navigationcomponentexample/FirstFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
 class FirstFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +17,14 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val root=inflater.inflate(R.layout.fragment_first, container, false)
+
+        val btnNavigate=root.findViewById<Button>(R.id.btnNavigate)
+        btnNavigate.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+
+
+        return root
     }
 }
